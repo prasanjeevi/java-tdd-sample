0b9f6910923dd628a13abbcb647a5e09199e7b20
sanjeevi <sanjeevi@AMB00865.local>
2017-12-20 15:54:03 +0530

    step3-green

--- src/main/java/com/company/samples/LeapYear.java
+++ src/main/java/com/company/samples/LeapYear.java
@@ -2,6 +2,6 @@ package com.company.samples;
 
 public class LeapYear {
     public static boolean isLeap(int year) {
-        return year % 4 == 0;
+        return year % 4 == 0 && !(year % 100 == 0);
     }
 }
