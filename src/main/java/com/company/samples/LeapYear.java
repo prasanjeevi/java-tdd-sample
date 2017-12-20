ac7a127cc524a82bf648dd2f2364947a1acf86ce
sanjeevi <sanjeevi@AMB00865.local>
2017-12-20 15:59:19 +0530

    step4-green

--- src/main/java/com/company/samples/LeapYear.java
+++ src/main/java/com/company/samples/LeapYear.java
@@ -2,7 +2,7 @@ package com.company.samples;
 
 public class LeapYear {
     public static boolean isLeap(int year) {
-        return isDivisible(year, 4) && !isDivisible(year, 100);
+        return isDivisible(year, 4) && (!isDivisible(year, 100) || isDivisible(year, 400));
     }
 
     private static boolean isDivisible(int year, int denominator) {
