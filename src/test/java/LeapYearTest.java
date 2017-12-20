42cd8bee973f3ddd70079f0053580f70ac1c3c3a
sanjeevi <sanjeevi@AMB00865.local>
2017-12-20 15:50:49 +0530

    step2-red

--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,12 +1,18 @@
 import com.company.samples.LeapYear;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LeapYearTest {
 
     @Test
     public void leapYearsAreDivisibleByFour() {
-        assertTrue(LeapYear.isLeap(2018));
+        assertTrue(LeapYear.isLeap(2016));
+    }
+
+    @Test
+    public void normalYearsAreNotDivisibleByFour() {
+        assertFalse(LeapYear.isLeap(2018));
     }
 }
